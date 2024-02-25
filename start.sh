93b764f6fceb3848943c4ff94a8b4f9036573a73
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2024-02-25 16:26:39 +0100

    Fixing variables name

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 clear
 
-INSTALL_DIR=/usr/local/turbolab.it/symfony-bundle-builder/
-RAW_REPO_URL=https://raw.githubusercontent.com/TurboLabIt/symfony-bundle-builder/main/
+SBB_INSTALL_DIR=/usr/local/turbolab.it/symfony-bundle-builder/
+SBB_RAW_REPO_URL=https://raw.githubusercontent.com/TurboLabIt/symfony-bundle-builder/main/
 
-if [ -f "${INSTALL_DIR}scripts/symfony-bundle-builder.sh" ]; then
-  source "${INSTALL_DIR}scripts/symfony-bundle-builder.sh"
+if [ -f "${SBB_INSTALL_DIR}scripts/symfony-bundle-builder.sh" ]; then
+  source "${SBB_INSTALL_DIR}scripts/symfony-bundle-builder.sh"
 else
-  source <(curl -s "${RAW_REPO_URL}script/symfony-bundle-builder.sh")
+  source <(curl -s "${SBB_RAW_REPO_URL}script/symfony-bundle-builder.sh")
 fi
