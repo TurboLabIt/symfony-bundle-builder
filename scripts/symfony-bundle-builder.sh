6341deaa3a8724febec7e82a5463c9d0a5df8d55
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2024-02-25 18:00:07 +0100

    Symfony Bundle Builder COMPLETED

---
+++ scripts/symfony-bundle-builder.sh
@@ -0,0 +1,198 @@
+## Startup procedure to build a Symfony Bundle
+# https://github.com/TurboLabIt/symfony-bundle-builder/blob/master/scripts/symfony-bundle-builder.sh
+#
+# How to:
+#
+# 1. Copy the "starter" script to your project directory with:
+#   curl -Lo scripts/symfony-bundle-builder.sh https://raw.githubusercontent.com/TurboLabIt/symfony-bundle-builder/master/my-app-template/scripts/symfony-bundle-builder.sh && sudo chmod u=rwx,go=rx scripts/*.sh
+#
+# 1. You should now git commit your copy
+
+SCRIPT_TITLE="📦 Symfony Bundle Builder"
+
+if [ -f "${SBB_INSTALL_DIR}scripts/symfony-bundle-script-begin.sh" ]; then
+  source "${SBB_INSTALL_DIR}scripts/symfony-bundle-script-begin.sh"
+else
+  source <(curl -s "${SBB_RAW_REPO_URL}script/symfony-bundle-script-begin.sh")
+fi
+
+
+if [ ! -z "${REQ_CHECK_FAILURE}" ]; then
+  fxCatastrophicError "Some required tools are missing!"
+fi
+
+
+fxTitle "🏢 Enter your VENDOR name"
+fxInfo "For example: \"TurboLabIt\". A-Z and 0-9 only"
+while [ -z "$SBB_BUNDLE_VENDOR_NAME" ]; do
+
+  echo "🤖 Provide the name"
+  read -p ">> " SBB_BUNDLE_VENDOR_NAME  < /dev/tty
+
+done
+
+fxOK "OK, the vendor name is ##$SBB_BUNDLE_VENDOR_NAME##"
+
+
+fxTitle "📦 Enter your PACKAGE name"
+fxInfo "For example: \"BaseCommand\". A-Z and 0-9 only, skip the word BUNDLE at the end"
+while [ -z "$SBB_BUNDLE_PACKAGE_NAME" ]; do
+
+  echo "🤖 Provide the name"
+  read -p ">> " SBB_BUNDLE_PACKAGE_NAME  < /dev/tty
+
+done
+
+fxOK "Acknowledged, ##$SBB_BUNDLE_PACKAGE_NAME## is going to rock"
+
+
+fxTitle "📦 Enter your DEVELOPER name"
+fxInfo "For example: \"Zane di TurboLab.it\". Anything but quotes goes"
+while [ -z "$SBB_DEVELOPER_NAME" ]; do
+
+  echo "🤖 Provide the name"
+  read -p ">> " SBB_DEVELOPER_NAME  < /dev/tty
+
+done
+
+fxOK "Nice to meet you, ##$SBB_DEVELOPER_NAME##"
+
+
+fxTitle "📦 Enter your EMAIL address"
+fxInfo "For example: \"info@turbolab.it\". Anything but quotes goes"
+while [ -z "$SBB_DEVELOPER_EMAIL" ]; do
+
+  echo "🤖 Provide your email"
+  read -p ">> " SBB_DEVELOPER_EMAIL  < /dev/tty
+
+done
+
+fxOK "Got it, ##$SBB_DEVELOPER_EMAIL##"
+
+
+fxTitle "Looking for composer.json..."
+if [ ! -f "${PROJECT_DIR}composer.json" ]; then
+
+  fxInfo "##${PROJECT_DIR}composer.json## not found. Downloading..."
+  getTemplateFile "composer.json"
+
+else
+
+  fxOK "composer.json found, nothing to do"
+fi
+
+replaceVendorPackageNameInFile ${PROJECT_DIR}composer.json
+
+
+symfony local:php:refresh
+symfony composer update
+rm -rf composer.lock
+
+
+fxTitle "Looking for a .gitignore..."
+if [ ! -f "${PROJECT_DIR}.gitignore" ]; then
+
+  fxInfo "##${PROJECT_DIR}.gitignore## not found. Downloading..."
+  curl -O https://raw.githubusercontent.com/TurboLabIt/webdev-gitignore/master/.gitignore
+
+else
+
+  fxOK ".gitignore found, nothing to do"
+fi
+
+
+fxTitle "Building the bundle structure..."
+## 📚 https://symfony.com/doc/current/bundles.html#bundle-directory-structure
+for DIR_NAME in assets config public templates translations scripts; do
+
+  if [ ! -d "${PROJECT_DIR}${DIR_NAME}" ]; then
+
+    fxInfo "##${PROJECT_DIR}${DIR_NAME}## folder not found. Creating..."
+    mkdir "${PROJECT_DIR}${DIR_NAME}"
+
+  else
+
+    fxOK "${DIR_NAME} found, nothing to do"
+  fi
+
+done
+
+
+fxTitle "Checking the main Bundle file..."
+if [ ! -f "${PROJECT_DIR}*Bundle.php" ]; then
+
+  fxInfo "No Bundle file found. Downloading..."
+  getTemplateFile MyVendorNameMyPackageNameBundle.php "${SBB_BUNDLE_VENDOR_NAME}${SBB_BUNDLE_PACKAGE_NAME}Bundle.php"
+
+else
+
+  fxOK "src found, nothing to do"
+fi
+
+replaceVendorPackageNameInFile "${SBB_BUNDLE_VENDOR_NAME}${SBB_BUNDLE_PACKAGE_NAME}Bundle.php"
+
+
+fxTitle "Checking config/..."
+if [ ! -f "${PROJECT_DIR}config/services.yaml" ]; then
+
+  fxInfo "##${PROJECT_DIR}config/services.yaml## not found. Downloading..."
+  cd config
+  getTemplateFile config/services.yaml
+  cd ..
+
+else
+
+  fxOK "services.yaml found, nothing to do"
+fi
+
+replaceVendorPackageNameInFile "config/services.yaml"
+
+
+fxTitle "Checking src/DependencyInjection/..."
+if [ ! -d "${PROJECT_DIR}src/DependencyInjection" ]; then
+
+  fxInfo "##${PROJECT_DIR}src/DependencyInjection## folder not found. Creating..."
+  mkdir -p src/Service
+  mkdir -p src/DependencyInjection
+  cd src/DependencyInjection
+  getTemplateFile src/DependencyInjection/MyVendorNameMyPackageNameExtension.php "${SBB_BUNDLE_VENDOR_NAME}${SBB_BUNDLE_PACKAGE_NAME}Extension.php"
+  cd ../..
+
+else
+
+  fxOK "src/DependencyInjection found, nothing to do"
+fi
+
+replaceVendorPackageNameInFile "src/DependencyInjection/${SBB_BUNDLE_VENDOR_NAME}${SBB_BUNDLE_PACKAGE_NAME}Extension.php"
+
+
+fxTitle "Checking scripts/symfony-bundle-tester.sh..."
+if [ ! -f "${PROJECT_DIR}scripts/symfony-bundle-tester.sh" ]; then
+
+  fxInfo "##${PROJECT_DIR}scripts/symfony-bundle-tester.sh## not found. Downloading..."
+  cd scripts
+  getTemplateFile scripts/symfony-bundle-tester.sh
+  cd ..
+
+else
+
+  fxOK "symfony-bundle-tester.sh found, nothing to do"
+fi
+
+
+## dependencies
+fxTitle "Looking for Symfony Framework..."
+if [ ! -d "${PROJECT_DIR}vendor/symfony/framework-bundle" ]; then
+
+  fxInfo "Symfony Framework not found. Composer req it now..."
+  symfony composer require symfony/framework-bundle --dev
+
+else
+
+  fxOK "Symfony Framework is already installed"
+fi
+
+rm -rf composer.lock
+
+
+fxEndFooter
---
+++ scripts/symfony-bundle-script-begin.sh
@@ -0,0 +1,83 @@
+## https://github.com/TurboLabIt/bash-fx
+if [ -f "/usr/local/turbolab.it/bash-fx/bash-fx.sh" ]; then
+  source "/usr/local/turbolab.it/bash-fx/bash-fx.sh"
+else
+  source <(curl -s https://raw.githubusercontent.com/TurboLabIt/bash-fx/main/bash-fx.sh)
+fi
+## bash-fx is ready
+
+fxHeader "${SCRIPT_TITLE}"
+
+
+PROJECT_DIR=$(pwd)/
+fxInfo "Working in ##${PROJECT_DIR}##"
+
+
+if [ -f "scripts/symfony-bundle-builder.sh" ]; then
+  fxCatastrophicError "You can only run this command from your own directory. RTFM: https://github.com/TurboLabIt/symfony-bundle-builder"
+fi
+
+
+fxTitle "Requirements check..."
+function checkReqCommand()
+{
+  if [ -z $(command -v $1) ]; then
+
+    echo "🛑 $1 is missing!"
+    REQ_CHECK_FAILURE=1
+
+  else
+
+    echo "✅ $1 is installed"
+  fi
+}
+
+checkReqCommand php
+checkReqCommand composer
+checkReqCommand symfony
+
+if [ -s "${PROJECT_DIR}.php-version" ]; then
+
+  echo "✅ .php-version exists. PHP version set to ##$(cat .php-version)##"
+
+else
+
+  echo "🛑 ##${PROJECT_DIR}.php-version## is missing or empty! It must contain the PHP version to use. This will build it for you:"
+  fxMessage "echo '8.3' > .php-version"
+  touch .php-version
+  REQ_CHECK_FAILURE=1
+fi
+
+
+function getTemplateFile()
+{
+  if [ -f "${SBB_INSTALL_DIR}template/$1" ] && [ -z "$2" ]; then
+
+    cp "${SBB_INSTALL_DIR}template/$1" .
+
+  elif [ -f "${SBB_INSTALL_DIR}template/$1" ]; then
+
+    cp "${SBB_INSTALL_DIR}template/$1" "$2"
+
+  elif [ ! -f "${SBB_INSTALL_DIR}template/$1" ] && [ -z "$2" ]; then
+
+    curl -O <(curl -s "${SBB_RAW_REPO_URL}template/$1")
+
+  else
+
+    curl -o "$2" <(curl -s "${SBB_RAW_REPO_URL}template/$1")
+  fi
+}
+
+
+function replaceVendorPackageNameInFile()
+{
+  sed -i "s|myvendorname/mypackagename|${SBB_BUNDLE_VENDOR_NAME,,}/${SBB_BUNDLE_PACKAGE_NAME,,}|g" "$1"
+  sed -i "s|MyVendorName\\\\\\\\MyPackageName|${SBB_BUNDLE_VENDOR_NAME}\\\\\\\\${SBB_BUNDLE_PACKAGE_NAME}|g" "$1"
+
+  sed -i "s|MyVendorName\\\\\\MyPackageName|${SBB_BUNDLE_VENDOR_NAME}\\\\\\${SBB_BUNDLE_PACKAGE_NAME}|g" "$1"
+  sed -i "s|MyVendorNameMyPackageName|${SBB_BUNDLE_VENDOR_NAME}${SBB_BUNDLE_PACKAGE_NAME}|g" "$1"
+
+  sed -i "s|MyDeveloperName|${SBB_DEVELOPER_NAME}|g" "$1"
+  sed -i "s|MyDeveloperEmailAddress|${SBB_DEVELOPER_EMAIL}|g" "$1"
+}
