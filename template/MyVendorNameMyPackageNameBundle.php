2122305221d12db947fd9852c3298e452c66b5e7
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2024-02-25 17:48:50 +0100

    template: bundle activator class

---
+++ template/MyVendorNameMyPackageNameBundle.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * 🪄 Based on https://github.com/TurboLabIt/symfony-bundle-builder/blob/main/template/MyVendorNameMyPackageNameBundle.php
+ *
+ * 📚 Usage example (customize with your own):
+ *
+ * - MyVendorName   ➡ TurboLabIt
+ * - MyPackageName  ➡ BaseCommand
+ *
+ * 💡 "Replace all" the above and you're ready to go
+ *
+ * 📚 Resources:
+ *
+ * - https://symfony.com/doc/current/bundles.html
+ * - https://symfonycasts.com/screencast/symfony-bundle/bundle-directory
+ */
+namespace MyVendorName\MyPackageNameBundle;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+
+class MyVendorNameMyPackageNameBundle extends AbstractBundle {}
